e9381943300d9b3f70bee6f026aea38b556b258f
Diogenes Santos <92751933+DG-9821@users.noreply.github.com>
2023-05-10 12:27:12 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -7,11 +7,12 @@ apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
-echo " Baixando e atualizando site"
+echo " Baixando e copiando arquivos da aplicação"
 
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
- echo "finalizado"
+
+ echo "Finalizado!!"
